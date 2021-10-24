4284bc61de24790fafef11f01e2c4a49e1cee805
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-10-24 01:39:53 -0300

    Config layout of components

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,5 +1,45 @@
+import styles from "./styles.module.scss";
+import LogoImg from "../../assets/logo.svg";
+
 function MessageList() {
-  return <h1>Message List</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={LogoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Estou muito ansioso por este evento, com certeza vai ser o melhor do
+            ano.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/DhBarboza.png"
+                alt="Douglas Henrique"
+              />
+            </div>
+            <span>Douglas Henrique</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Estou muito ansioso por este evento, com certeza vai ser o melhor do
+            ano.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/DhBarboza.png"
+                alt="Douglas Henrique"
+              />
+            </div>
+            <span>Douglas Henrique</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export { MessageList };
