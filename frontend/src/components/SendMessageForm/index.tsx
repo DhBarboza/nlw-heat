a2adf98224942feb1e29ca1949445b46ca226bff
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-10-26 12:05:11 -0300

    Add component Send Message Form

--- frontend/src/components/SendMessageForm/index.tsx
+++ frontend/src/components/SendMessageForm/index.tsx
@@ -1,3 +1,12 @@
-export function SendMessageForm() {
-  
+import { VscSignOut } from "react-icons/vsc";
+import styles from "./styles.module.scss";
+
+function SendMessageForm() {
+  return <div className={styles.sendMessageFormWrapper}>
+    <button className={styles.signOutButton}>
+      <VscSignOut />
+    </button>
+  </div>;
 }
+
+export { SendMessageForm };
